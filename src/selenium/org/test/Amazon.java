96129609c567de4859e78767248cd742dd383fbd
indujaudhayak <indujaudhayak@gmail.com>
2019-05-07 09:13:17 -0400

    First project

---
+++ src/selenium/org/test/Amazon.java
@@ -0,0 +1,13 @@
+package selenium.org.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Amazon {
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\USER\\eclipse-workspace\\selenium2\\webdriver\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.amazon.in");
+	}  
+
+}
